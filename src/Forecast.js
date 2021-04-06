f3efb44332b3282a7345804966c5d0f5c02ab3ed
momopeachyh <76701058+momopeachyh@users.noreply.github.com>
2021-04-05 21:48:13 -0500

    Fix

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,15 +1,3 @@
-import React from "react";
-import axios from "axios";
-
 export default function Forecast(props) {
   return "chicago";
-
-  function getForecast(response) {
-    console.log(response);
-  }
-
-  const apiKey = "343320b5e251ee7c39260263367d8fb5";
-  const units = "metric";
-  let url = `https://api.openweathermap.org/data/2.5/forecast?q=${props.city}&appid=${apiKey}&units=${units}`;
-  axios.get(url).then(getForecast);
 }
